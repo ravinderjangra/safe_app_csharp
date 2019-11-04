e0c1b52327c817b894096c2234e7aefda9313ac8
RavinderJangra <ravinderjangra@live.com>
2019-11-04 14:48:25 +0530

    tests/logging: update log.toml file, include rustlogger and config path test in MiscTest

--- Tests/SafeApp.Tests/MiscTest.cs
+++ Tests/SafeApp.Tests/MiscTest.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SafeApp.Core;
 using SafeApp.MockAuthBindings;
 
 namespace SafeApp.Tests
@@ -13,5 +18,41 @@ namespace SafeApp.Tests
         [Test]
         public void IsMockSafeAppBuildTest()
             => Assert.That(Session.IsMockBuild(), Is.True);
+
+        [Test]
+        public async Task SetConfigFileDirPathTest()
+            => await Session.SetConfigurationFilePathAsync(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+        [Test]
+        public async Task RustLoggerTest()
+        {
+            var configPath = string.Empty;
+            Assert.That(async () => configPath = await TestUtils.InitRustLogging(), Throws.Nothing);
+            Assert.That(
+                async () =>
+                await Session.DecodeIpcMessageAsync("Some Random Invalid String"),
+                Throws.TypeOf<IpcMsgException>());
+            var fileEmpty = true;
+            for (var i = 0; i < 10; ++i)
+            {
+                await Task.Delay(1000);
+                using (var fs = new FileStream(
+                    Path.Combine(configPath, "Client.log"),
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs, Encoding.Default))
+                {
+                    fileEmpty = string.IsNullOrEmpty(sr.ReadToEnd());
+                    if (!fileEmpty)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Assert.That(fileEmpty, Is.False);
+        }
     }
 }
