d6d5066a6aba5aba2ea83cec825cd6a0ebec82e7
Ravinder Jangra <ravinderjangra@live.com>
2020-05-20 12:59:16 +0530

    authenticator project refactored

--- SafeAuthenticator/AuthBindings.cs
+++ SafeAuthenticator/AuthBindings.cs
@@ -18,261 +18,91 @@ namespace SafeAuthenticator
         private const string DllName = "safe_api";
 #endif
 
-        public bool IsMockBuild()
+        public Task<IntPtr> LogInAsync(string passphrase, string password)
         {
-            var ret = AuthIsMockNative();
+            var (ret, userData) = BindingUtils.PrepareTask<IntPtr>();
+            LogInNative(passphrase, password, userData, DelegateOnFfiResultSafeAuthenticatorCb);
             return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_is_mock")]
-        private static extern bool AuthIsMockNative();
+        [DllImport(DllName, EntryPoint = "log_in")]
+        private static extern void LogInNative([MarshalAs(UnmanagedType.LPStr)] string passphrase, [MarshalAs(UnmanagedType.LPStr)] string password, IntPtr userData, FfiResultSafeAuthenticatorCb oCb);
 
-        [DllImport(DllName, EntryPoint = "create_client_with_acc")]
-        private static extern void CreateAccNative(
-            [MarshalAs(UnmanagedType.LPStr)] string accountLocator,
-            [MarshalAs(UnmanagedType.LPStr)] string accountPassword,
-            IntPtr userData,
-            NoneCb oDisconnectNotifierCb,
-            FfiResultAuthenticatorCb oCb);
-
-        [DllImport(DllName, EntryPoint = "login")]
-        private static extern void LoginNative(
-            [MarshalAs(UnmanagedType.LPStr)] string accountLocator,
-            [MarshalAs(UnmanagedType.LPStr)] string accountPassword,
-            IntPtr userData,
-            NoneCb oDisconnectNotifierCb,
-            FfiResultAuthenticatorCb oCb);
-
-        public Task AuthReconnectAsync(IntPtr auth)
+        public Task LogOutAsync(IntPtr app)
         {
             var (ret, userData) = BindingUtils.PrepareTask();
-            AuthReconnectNative(auth, userData, DelegateOnFfiResultCb);
+            LogOutNative(app, userData, DelegateOnFfiResultCb);
             return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_reconnect")]
-        private static extern void AuthReconnectNative(IntPtr auth, IntPtr userData, FfiResultCb oCb);
+        [DllImport(DllName, EntryPoint = "log_out")]
+        private static extern void LogOutNative(IntPtr app, IntPtr userData, FfiResultCb oCb);
 
-        public void AuthFree(IntPtr auth)
+        public Task<bool> IsLoggedInAsync(IntPtr app)
         {
-            AuthFreeNative(auth);
+            var (ret, userData) = BindingUtils.PrepareTask<bool>();
+            IsLoggedInNative(app, userData, DelegateOnFfiResultBoolCb);
+            return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_free")]
-        private static extern void AuthFreeNative(IntPtr auth);
+        [DllImport(DllName, EntryPoint = "is_logged_in")]
+        private static extern void IsLoggedInNative(IntPtr app, IntPtr userData, FfiResultBoolCb oCb);
 
-        public Task AuthRmRevokedAppAsync(IntPtr auth, string appId)
+        public Task<IntPtr> CreateAccAsync(string secretKey, string passphrase, string password)
+        {
+            var (ret, userData) = BindingUtils.PrepareTask<IntPtr>();
+            CreateAccNative(secretKey, passphrase, password, userData, DelegateOnFfiResultSafeAuthenticatorCb);
+            return ret;
+        }
+
+        [DllImport(DllName, EntryPoint = "create_acc")]
+        private static extern void CreateAccNative([MarshalAs(UnmanagedType.LPStr)] string secretKey, [MarshalAs(UnmanagedType.LPStr)] string passphrase, [MarshalAs(UnmanagedType.LPStr)] string password, IntPtr userData, FfiResultSafeAuthenticatorCb oCb);
+
+        public Task<string> AutheriseAppAsync(IntPtr app, string request, bool isGranted)
+        {
+            var (ret, userData) = BindingUtils.PrepareTask<string>();
+            AutheriseAppNative(app, request, isGranted, userData, DelegateOnFfiResultStringCb);
+            return ret;
+        }
+
+        [DllImport(DllName, EntryPoint = "autherise_app")]
+        private static extern void AutheriseAppNative(IntPtr app, [MarshalAs(UnmanagedType.LPStr)] string request, [MarshalAs(UnmanagedType.U1)] bool isGranted, IntPtr userData, FfiResultStringCb oCb);
+
+        public Task RevokeAppAsync(IntPtr app, string appId)
         {
             var (ret, userData) = BindingUtils.PrepareTask();
-            AuthRmRevokedAppNative(auth, appId, userData, DelegateOnFfiResultCb);
+            RevokeAppNative(app, appId, userData, DelegateOnFfiResultCb);
             return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_rm_revoked_app")]
-        private static extern void AuthRmRevokedAppNative(
-            IntPtr auth,
-            [MarshalAs(UnmanagedType.LPStr)] string appId,
-            IntPtr userData,
-            FfiResultCb oCb);
+        [DllImport(DllName, EntryPoint = "revoke_app")]
+        private static extern void RevokeAppNative(IntPtr app, [MarshalAs(UnmanagedType.LPStr)] string appId, IntPtr userData, FfiResultCb oCb);
 
-        public Task<List<AppExchangeInfo>> AuthRevokedAppsAsync(IntPtr auth)
+        public Task<List<AuthedApp>> AuthdAppAsync(IntPtr app)
         {
-            var (ret, userData) = BindingUtils.PrepareTask<List<AppExchangeInfo>>();
-            AuthRevokedAppsNative(auth, userData, DelegateOnFfiResultAppExchangeInfoListCb);
+            var (ret, userData) = BindingUtils.PrepareTask<List<AuthedApp>>();
+            AuthdAppNative(app, userData, DelegateOnFfiResultAuthedAppListCb);
             return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_revoked_apps")]
-        private static extern void AuthRevokedAppsNative(IntPtr auth, IntPtr userData, FfiResultAppExchangeInfoListCb oCb);
+        [DllImport(DllName, EntryPoint = "authd_app")]
+        private static extern void AuthdAppNative(IntPtr app, IntPtr userData, FfiResultAuthedAppListCb oCb);
 
-        public Task<List<RegisteredApp>> AuthRegisteredAppsAsync(IntPtr auth)
-        {
-            var (ret, userData) = BindingUtils.PrepareTask<List<RegisteredApp>>();
-            AuthRegisteredAppsNative(auth, userData, DelegateOnFfiResultRegisteredAppListCb);
-            return ret;
-        }
+        [DllImport(DllName, EntryPoint = "decode_req")]
+        private static extern void DecodeReqNative(IntPtr app, [MarshalAs(UnmanagedType.LPStr)] string msg, IntPtr userData, UIntAuthReqCb oAuth, UIntContainersReqCb oContainers, UIntByteListCb oUnregistered, FfiResultCb oErr);
 
-        [DllImport(DllName, EntryPoint = "auth_registered_apps")]
-        private static extern void AuthRegisteredAppsNative(IntPtr auth, IntPtr userData, FfiResultRegisteredAppListCb oCb);
+        [DllImport(DllName, EntryPoint = "decode_auth_unregistered_req")]
+        private static extern void DecodeAuthUnregisteredReqNative([MarshalAs(UnmanagedType.LPStr)] string msg, IntPtr userData, UIntByteListCb oUnregistered, FfiResultCb oErr);
 
-        public Task<List<AppAccess>> AuthAppsAccessingMutableDataAsync(IntPtr auth, byte[] mdName, ulong mdTypeTag)
-        {
-            var (ret, userData) = BindingUtils.PrepareTask<List<AppAccess>>();
-            AuthAppsAccessingMutableDataNative(auth, mdName, mdTypeTag, userData, DelegateOnFfiResultAppAccessListCb);
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "auth_apps_accessing_mutable_data")]
-        private static extern void AuthAppsAccessingMutableDataNative(
-            IntPtr auth,
-            [MarshalAs(UnmanagedType.LPArray, SizeConst = (int)AppConstants.XorNameLen)]
-            byte[] mdName,
-            ulong mdTypeTag,
-            IntPtr userData,
-            FfiResultAppAccessListCb oCb);
-
-        [DllImport(DllName, EntryPoint = "auth_unregistered_decode_ipc_msg")]
-        private static extern void AuthUnregisteredDecodeIpcMsgNative(
-            [MarshalAs(UnmanagedType.LPStr)] string msg,
-            IntPtr userData,
-            UIntByteListCb oUnregistered,
-            FfiResultStringCb oErr);
-
-        [DllImport(DllName, EntryPoint = "auth_decode_ipc_msg")]
-        private static extern void AuthDecodeIpcMsgNative(
-            IntPtr auth,
-            [MarshalAs(UnmanagedType.LPStr)] string msg,
-            IntPtr userData,
-            UIntAuthReqCb oAuth,
-            UIntContainersReqCb oContainers,
-            UIntByteListCb oUnregistered,
-            UIntShareMDataReqMetadataResponseListCb oShareMData,
-            FfiResultStringCb oErr);
-
-        public Task<string> EncodeShareMDataRespAsync(IntPtr auth, ref ShareMDataReq req, uint reqId, bool isGranted)
-        {
-            var reqNative = req.ToNative();
-            var (ret, userData) = BindingUtils.PrepareTask<string>();
-            EncodeShareMDataRespNative(auth, ref reqNative, reqId, isGranted, userData, DelegateOnFfiResultStringCb);
-            reqNative.Free();
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "encode_share_mdata_resp")]
-        private static extern void EncodeShareMDataRespNative(
-            IntPtr auth,
-            ref ShareMDataReqNative req,
-            uint reqId,
-            [MarshalAs(UnmanagedType.U1)] bool isGranted,
-            IntPtr userData,
-            FfiResultStringCb oCb);
-
-        public Task<string> AuthRevokeAppAsync(IntPtr auth, string appId)
+        public Task<string> AutheriseUnregisteredAppAsync(uint reqId, bool isGranted)
         {
             var (ret, userData) = BindingUtils.PrepareTask<string>();
-            AuthRevokeAppNative(auth, appId, userData, DelegateOnFfiResultStringCb);
+            AutheriseUnregisteredAppNative(reqId, isGranted, userData, DelegateOnFfiResultStringCb);
             return ret;
         }
 
-        [DllImport(DllName, EntryPoint = "auth_revoke_app")]
-        private static extern void AuthRevokeAppNative(
-            IntPtr auth,
-            [MarshalAs(UnmanagedType.LPStr)] string appId,
-            IntPtr userData,
-            FfiResultStringCb oCb);
-
-        public Task AuthFlushAppRevocationQueueAsync(IntPtr auth)
-        {
-            var (ret, userData) = BindingUtils.PrepareTask();
-            AuthFlushAppRevocationQueueNative(auth, userData, DelegateOnFfiResultCb);
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "auth_flush_app_revocation_queue")]
-        private static extern void AuthFlushAppRevocationQueueNative(IntPtr auth, IntPtr userData, FfiResultCb oCb);
-
-        public Task<string> EncodeUnregisteredRespAsync(uint reqId, bool isGranted)
-        {
-            var (ret, userData) = BindingUtils.PrepareTask<string>();
-            EncodeUnregisteredRespNative(reqId, isGranted, userData, DelegateOnFfiResultStringCb);
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "encode_unregistered_resp")]
-        private static extern void EncodeUnregisteredRespNative(
-            uint reqId,
-            [MarshalAs(UnmanagedType.U1)] bool isGranted,
-            IntPtr userData,
-            FfiResultStringCb oCb);
-
-        public Task<string> EncodeAuthRespAsync(IntPtr auth, ref AuthReq req, uint reqId, bool isGranted)
-        {
-            var reqNative = req.ToNative();
-            var (ret, userData) = BindingUtils.PrepareTask<string>();
-            EncodeAuthRespNative(auth, ref reqNative, reqId, isGranted, userData, DelegateOnFfiResultStringCb);
-            reqNative.Free();
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "encode_auth_resp")]
-        private static extern void EncodeAuthRespNative(
-            IntPtr auth,
-            ref AuthReqNative req,
-            uint reqId,
-            [MarshalAs(UnmanagedType.U1)] bool isGranted,
-            IntPtr userData,
-            FfiResultStringCb oCb);
-
-        public Task<string> EncodeContainersRespAsync(IntPtr auth, ref ContainersReq req, uint reqId, bool isGranted)
-        {
-            var reqNative = req.ToNative();
-            var (ret, userData) = BindingUtils.PrepareTask<string>();
-            EncodeContainersRespNative(auth, ref reqNative, reqId, isGranted, userData, DelegateOnFfiResultStringCb);
-            reqNative.Free();
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "encode_containers_resp")]
-        private static extern void EncodeContainersRespNative(
-            IntPtr auth,
-            ref ContainersReqNative req,
-            uint reqId,
-            [MarshalAs(UnmanagedType.U1)] bool isGranted,
-            IntPtr userData,
-            FfiResultStringCb oCb);
-
-        public Task AuthInitLoggingAsync(string outputFileNameOverride)
-        {
-            var (ret, userData) = BindingUtils.PrepareTask();
-            AuthInitLoggingNative(outputFileNameOverride, userData, DelegateOnFfiResultCb);
-            return ret;
-        }
-
-        [DllImport(DllName, EntryPoint = "auth_init_logging")]
-        private static extern void AuthInitLoggingNative(
-            [MarshalAs(UnmanagedType.LPStr)] string outputFileNameOverride,
-            IntPtr userData,
-            FfiResultCb oCb);
-
-        private delegate void FfiResultAppAccessListCb(IntPtr userData, IntPtr result, IntPtr appAccessPtr, UIntPtr appAccessLen);
-
-#if __IOS__
-        [MonoPInvokeCallback(typeof(FfiResultAppAccessListCb))]
-#endif
-        private static void OnFfiResultAppAccessListCb(IntPtr userData, IntPtr result, IntPtr appAccessPtr, UIntPtr appAccessLen)
-        {
-            BindingUtils.CompleteTask(
-                userData,
-                Marshal.PtrToStructure<FfiResult>(result),
-                () => BindingUtils.CopyToObjectList<AppAccess>(appAccessPtr, (int)appAccessLen));
-        }
-
-        private static readonly FfiResultAppAccessListCb DelegateOnFfiResultAppAccessListCb = OnFfiResultAppAccessListCb;
-
-        private delegate void FfiResultAppExchangeInfoListCb(
-            IntPtr userData,
-            IntPtr result,
-            IntPtr appExchangeInfoPtr,
-            UIntPtr appExchangeInfoLen);
-
-#if __IOS__
-        [MonoPInvokeCallback(typeof(FfiResultAppExchangeInfoListCb))]
-#endif
-        private static void OnFfiResultAppExchangeInfoListCb(
-            IntPtr userData,
-            IntPtr result,
-            IntPtr appExchangeInfoPtr,
-            UIntPtr appExchangeInfoLen)
-        {
-            BindingUtils.CompleteTask(
-                userData,
-                Marshal.PtrToStructure<FfiResult>(result),
-                () => BindingUtils.CopyToObjectList<AppExchangeInfo>(appExchangeInfoPtr, (int)appExchangeInfoLen));
-        }
-
-        private static readonly FfiResultAppExchangeInfoListCb DelegateOnFfiResultAppExchangeInfoListCb = OnFfiResultAppExchangeInfoListCb;
-
-        private delegate void FfiResultAuthenticatorCb(IntPtr userData, IntPtr result, IntPtr authenticator);
+        [DllImport(DllName, EntryPoint = "autherise_unregistered_app")]
+        private static extern void AutheriseUnregisteredAppNative(uint reqId, [MarshalAs(UnmanagedType.U1)] bool isGranted, IntPtr userData, FfiResultStringCb oCb);
 
         private delegate void FfiResultCb(IntPtr userData, IntPtr result);
 
@@ -286,55 +116,62 @@ namespace SafeAuthenticator
 
         private static readonly FfiResultCb DelegateOnFfiResultCb = OnFfiResultCb;
 
-        private delegate void FfiResultRegisteredAppListCb(
-            IntPtr userData,
-            IntPtr result,
-            IntPtr registeredAppPtr,
-            UIntPtr registeredAppLen);
+        private delegate void FfiResultSafeAuthenticatorCb(IntPtr userData, IntPtr result, IntPtr auth);
 
 #if __IOS__
-        [MonoPInvokeCallback(typeof(FfiResultRegisteredAppListCb))]
+        [MonoPInvokeCallback(typeof(FfiResultSafeAuthenticatorCb))]
 #endif
-        private static void OnFfiResultRegisteredAppListCb(
-            IntPtr userData,
-            IntPtr result,
-            IntPtr registeredAppPtr,
-            UIntPtr registeredAppLen)
+        private static void OnFfiResultSafeAuthenticatorCb(IntPtr userData, IntPtr result, IntPtr auth)
         {
-            BindingUtils.CompleteTask(
-                userData,
-                Marshal.PtrToStructure<FfiResult>(result),
-                () => BindingUtils.CopyToObjectList<RegisteredAppNative>(registeredAppPtr, (int)registeredAppLen).
-                    Select(native => new RegisteredApp(native)).ToList());
+            BindingUtils.CompleteTask(userData, Marshal.PtrToStructure<FfiResult>(result), () => auth);
         }
 
-        private static readonly FfiResultRegisteredAppListCb DelegateOnFfiResultRegisteredAppListCb = OnFfiResultRegisteredAppListCb;
+        private static readonly FfiResultSafeAuthenticatorCb DelegateOnFfiResultSafeAuthenticatorCb = OnFfiResultSafeAuthenticatorCb;
 
-        private delegate void FfiResultStringCb(IntPtr userData, IntPtr result, string response);
+        private delegate void FfiResultAuthedAppListCb(IntPtr userData, IntPtr result, IntPtr appsPtr, UIntPtr appsLen);
+
+#if __IOS__
+        [MonoPInvokeCallback(typeof(FfiResultAuthedAppListCb))]
+#endif
+        private static void OnFfiResultAuthedAppListCb(IntPtr userData, IntPtr result, IntPtr appsPtr, UIntPtr appsLen)
+        {
+            BindingUtils.CompleteTask(userData, Marshal.PtrToStructure<FfiResult>(result), () => BindingUtils.CopyToObjectList<AuthedAppNative>(appsPtr, (int)appsLen).Select(native => new AuthedApp(native)).ToList());
+        }
+
+        private static readonly FfiResultAuthedAppListCb DelegateOnFfiResultAuthedAppListCb = OnFfiResultAuthedAppListCb;
+
+        private delegate void FfiResultStringCb(IntPtr userData, IntPtr result, string authResponse);
 
 #if __IOS__
         [MonoPInvokeCallback(typeof(FfiResultStringCb))]
 #endif
-        private static void OnFfiResultStringCb(IntPtr userData, IntPtr result, string response)
+        private static void OnFfiResultStringCb(IntPtr userData, IntPtr result, string authResponse)
         {
-            BindingUtils.CompleteTask(userData, Marshal.PtrToStructure<FfiResult>(result), () => response);
+            BindingUtils.CompleteTask(userData, Marshal.PtrToStructure<FfiResult>(result), () => authResponse);
         }
 
         private static readonly FfiResultStringCb DelegateOnFfiResultStringCb = OnFfiResultStringCb;
 
+        private delegate void FfiResultBoolCb(IntPtr userData, IntPtr result, bool loggedIn);
+
+#if __IOS__
+        [MonoPInvokeCallback(typeof(FfiResultBoolCb))]
+#endif
+        private static void OnFfiResultBoolCb(IntPtr userData, IntPtr result, bool loggedIn)
+        {
+            BindingUtils.CompleteTask(userData, Marshal.PtrToStructure<FfiResult>(result), () => loggedIn);
+        }
+
         private delegate void NoneCb(IntPtr userData);
+
+        private static readonly FfiResultBoolCb DelegateOnFfiResultBoolCb = OnFfiResultBoolCb;
 
         private delegate void UIntAuthReqCb(IntPtr userData, uint reqId, IntPtr req);
 
-        private delegate void UIntByteListCb(IntPtr userData, uint reqId, IntPtr extraDataPtr, UIntPtr extraDataLen);
+        private delegate void UIntByteListCb(IntPtr userData, uint reqId, IntPtr serialisedCfgPtr, UIntPtr serialisedCfgLen);
+
+        private delegate void UIntCb(IntPtr userData, uint reqId);
 
         private delegate void UIntContainersReqCb(IntPtr userData, uint reqId, IntPtr req);
-
-        private delegate void UIntShareMDataReqMetadataResponseListCb(
-            IntPtr userData,
-            uint reqId,
-            IntPtr req,
-            IntPtr metadataPtr,
-            UIntPtr metadataLen);
     }
 }
