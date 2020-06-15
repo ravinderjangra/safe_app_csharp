d7ec2ef5e5eeb413bd4e06908625dd8dd52c1a12
Ravinder Jangra <ravinderjangra@live.com>
2020-06-15 14:45:01 +0530

    chore/ci: update mono, macOS host and cake version
    chore/ci: fix host not found issue

---
+++ Build/host_fix.sh
@@ -0,0 +1,7 @@
+echo "\nHosts before:"
+cat /etc/hosts
+echo "\nlocalhost:"
+scutil --get LocalHostName
+sudo -- sh -c "echo 127.0.0.1 $(scutil --get LocalHostName).local >> /etc/hosts"
+printf "\nHosts after:"
+cat /etc/hosts
