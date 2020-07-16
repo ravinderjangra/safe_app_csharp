e09cddf71b11948e90b90223b3b8218e312238f5
Ravinder Jangra <ravinderjangra@live.com>
2020-07-16 19:12:35 +0530

    docs/API: add xml doc comments for AuthTypes

--- SafeAuthenticator/AuthTypes.cs
+++ SafeAuthenticator/AuthTypes.cs
@@ -5,23 +5,64 @@ using SafeApp.Core;
 
 namespace SafeAuthenticator
 {
+    /// <summary>
+    /// App permissions.
+    /// </summary>
     public struct AppPermissions
     {
+        /// <summary>
+        /// Set true, if the app is allowed to transfer coins.
+        /// </summary>
         [MarshalAs(UnmanagedType.U1)]
         public bool TransferCoins;
+
+        /// <summary>
+        /// Set true, if the app is allowed to mutate data.
+        /// </summary>
         [MarshalAs(UnmanagedType.U1)]
         public bool PerformMutations;
+
+        /// <summary>
+        /// Set true, if the app is allowed to get balance.
+        /// </summary>
         [MarshalAs(UnmanagedType.U1)]
         public bool GetBalance;
     }
 
+    /// <summary>
+    /// Authenticated app information.
+    /// </summary>
     public struct AuthedApp
     {
+        /// <summary>
+        /// Application identifier.
+        /// </summary>
         public string Id;
+
+        /// <summary>
+        /// Application name.
+        /// </summary>
         public string Name;
+
+        /// <summary>
+        /// Application provider/vendor (e.g. MaidSafe).
+        /// </summary>
         public string Vendor;
+
+        /// <summary>
+        /// AppPermissions for the app.
+        /// </summary>
         public AppPermissions AppPermissions;
+
+        /// <summary>
+        /// The list of container permissions for the app.
+        /// </summary>
         public List<ContainerPermissions> Containers;
+
+        /// <summary>
+        /// Set true, if the app has a dedicated container for itself.
+        /// false otherwise.
+        /// </summary>
         public bool OwnContainer;
 
         internal AuthedApp(AuthedAppNative native)
